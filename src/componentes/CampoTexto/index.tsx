654e7b1a222fb7d58db9b1b81a94bc21165ca83c
Evandro <096066330647@tre-pr.jus.br>
2023-01-30 17:24:07 -0300

    Configurando tipagem de funções e desconstruindo props

---
+++ src/componentes/CampoTexto/index.tsx
@@ -0,0 +1,26 @@
+import './CampoTexto.css';
+
+interface CampoTextoProps {
+    aoAlterado: (valor: string) => void;
+    placeholder: string;
+    label: string;
+    valor: string;
+    obrigatorio: boolean;
+}
+
+const CampoTexto = ({aoAlterado, label, obrigatorio, placeholder, valor}: CampoTextoProps) => {
+
+    const aoDigitado = (evento : React.ChangeEvent<HTMLInputElement>) => {
+        aoAlterado(evento.target.value);
+    }
+
+    return (
+        <div className="campo-texto">
+            <label>{label}</label>
+            <input onChange={aoDigitado} required={obrigatorio} placeholder={placeholder}
+                value={valor}/>
+        </div>
+    )
+}
+
+export default CampoTexto;
